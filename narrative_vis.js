1536ad0cacf5d673fc06ad659ea9ed831dc744b8
nastyas76 <asushkova01@gmail.com>
2023-08-01 18:39:00 -0700

    removing the second data var

--- narrative_vis.js
+++ narrative_vis.js
@@ -14,18 +14,3 @@ d3.csv("https://nastyas76.github.io/narrative_vis/adult_census.csv").then(functi
     // For example, let's log the data to the console:
     console.log(data);
   }
-
-
-var data = [4, 8, 15, 16, 23, 42];
-document.getElementById("data").innerHTML= data;
-d3.select(".chart")
-  .selectAll("rect")
-  .data(data)
-  .enter().append("rect")
-  .attr("width", 19)
-  .attr("x", function(d, i) { return 20 * i; })
-  .attr("height", 0) 
-  .attr("y", 420) 
-  .transition().delay(500).duration(5000)
-  .attr("height", function(d) { return 10 * d; }) 
-  .attr("y", function(d) { return 420 - 10 * d; }); 
