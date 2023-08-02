8f413d8169a0a101909ea758084b272c81c9601a
nastyas76 <asushkova01@gmail.com>
2023-08-01 18:28:14 -0700

    fix on transition by setting height to 0

--- narrative_vis.js
+++ narrative_vis.js
@@ -1,13 +1,13 @@
 var data = [4, 8, 15, 16, 23, 42];
 document.getElementById("data").innerHTML= data;
 d3.select(".chart")
-    .selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("width", 19)
-    .attr("x", function(d,i) {return 20*i;})
-    .attr("height", 20)
-    .attr("y", 400)
-    .transition().duration(5000)
-    .attr("height", function(d) { return 10*d; })
-    .attr("y", function(d) { return 420 - 10*d; });
+  .selectAll("rect")
+  .data(data)
+  .enter().append("rect")
+  .attr("width", 19)
+  .attr("x", function(d, i) { return 20 * i; })
+  .attr("height", 0) 
+  .attr("y", 420) 
+  .transition().delay(500).duration(5000)
+  .attr("height", function(d) { return 10 * d; }) // Set the target height
+  .attr("y", function(d) { return 420 - 10 * d; }); // Set the target y position
