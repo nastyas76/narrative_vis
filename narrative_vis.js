238d4af7b7da7bd24913c1770e5fd0a98694f67d
nastyas76 <asushkova01@gmail.com>
2023-08-01 18:36:41 -0700

    attempt to read in csv file

--- narrative_vis.js
+++ narrative_vis.js
@@ -1,3 +1,21 @@
+
+
+d3.csv("https://nastyas76.github.io/narrative_vis/adult_census.csv").then(function(data) {
+    processData(data);
+  }).catch(function(error) {
+    console.error("Error loading CSV file:", error);
+  });
+  
+  function processData(data) {
+    // Here, you can process the data as needed
+    // 'data' will be an array of objects, where each object represents a row in the CSV file
+    // You can access specific columns using object properties
+  
+    // For example, let's log the data to the console:
+    console.log(data);
+  }
+
+
 var data = [4, 8, 15, 16, 23, 42];
 document.getElementById("data").innerHTML= data;
 d3.select(".chart")
@@ -9,5 +27,5 @@ d3.select(".chart")
   .attr("height", 0) 
   .attr("y", 420) 
   .transition().delay(500).duration(5000)
-  .attr("height", function(d) { return 10 * d; }) // Set the target height
-  .attr("y", function(d) { return 420 - 10 * d; }); // Set the target y position
+  .attr("height", function(d) { return 10 * d; }) 
+  .attr("y", function(d) { return 420 - 10 * d; }); 
