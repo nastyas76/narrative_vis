b11f9b7c28fb5e24f554b2f7a4ab28f1a9898ae7
nastyas76 <asushkova01@gmail.com>
2023-08-03 03:29:51 -0700

    testing

--- world.js
+++ world.js
@@ -1,105 +1,261 @@
-const margin = { top: 10, right: 10, bottom: 20, left: 20 };
-const height = 500;
-const width = 700;
 
-async function init () {
-    data = await d3.csv("https://nastyas76.github.io/narrative_vis/world-data-23-adjusted.csv", function(data) {
-    console.log(data);
+var values = ['Co2-Emissions', 'Out of pocket health expenditure', 'Urban_population']
+
+var GDPSlider = document.getElementById("slider");
+var categorySelector = document.getElementById("category");
+
+var index = 0;
+
+d3.csv("https://nastyas76.github.io/narrative_vis/world-data-23-adjusted.csv").then(function (data) {
 
 
-    x = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d['Co2-Emissions'])])
-        .range([margin.left, width - margin.right])
-        .nice();
+    var min = Math.min.apply(null, data.map(function (a) { return a.GDP; }))
+        , max = Math.max.apply(null, data.map(function (a) { return a.GDP; }))
 
-    y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d['Life expectancy'])])
-        .range([height - margin.bottom, margin.top])
-        .nice();
 
-    color = d3.scaleOrdinal()
-        .domain(data.map(d => d.Country))
-        .range(d3.schemeTableau10);
 
-    size = d3.scaleSqrt()
-        .domain(d3.extent(data, d => d.GDP))
-        .range([4, 35]);
+    displayChart(data, values[index]);
 
-    const svg = d3.create('svg')
-        .attr('width', width)
-        .attr('height', height);
+    var previousButton = document.getElementById("previousButton");
+    var nextButton = document.getElementById("nextButton");
 
-    svg.append('g')
-        .attr('transform', `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(x))
-        // Add x-axis title 'text' element.
-        .append('text')
-        .attr('text-anchor', 'end')
-        .attr('fill', 'black')
-        .attr('font-size', '12px')
-        .attr('font-weight', 'bold')
+    previousButton.classList.add("hidden")
+
+    previousButton.addEventListener('click', () => {
+        index--;
+        displayChart(data, values[index]);
+        if (index <= 0) {
+            index = 0;
+            previousButton.classList.add("hidden")
+            document.getElementById("forth").classList.remove("hidden");
+        }
+        if (index < 3) {
+            // hide div
+            nextButton.classList.remove("hidden")
+            document.getElementById("forth").classList.add("hidden");
+        }
+    })
+
+    nextButton.addEventListener('click', () => {
+        index++;
+        displayChart(data, values[index]);
+        if (index >= 3) {
+            index = 3;
+            GDPSlider.min = min;
+            GDPSlider.max = max;
+            nextButton.classList.add("hidden")
+            document.getElementById("forth").classList.remove("hidden");
+            console.log("should be displayed")
+        }
+        if (index > 0 && index < 3) {
+            previousButton.classList.remove("hidden")
+            document.getElementById("forth").classList.add("hidden");
+        }
+    })
+
+    GDPSlider.addEventListener("change", function () {
+        document.getElementById("rangevalue").innerHTML = GDPSlider.value;
+        displayChart(data, values[index])
+    }, false);
+
+    categorySelector.addEventListener("change", function () {
+        index = categorySelector.value;
+        displayChart(data, values[index])
+    }, false);
+
+
+});
+
+
+function displayChart(values, property) {
+    var minGDP = 0
+    if (!document.getElementById("forth").classList.contains("hidden")) {
+        minGDP = GDPSlider.value
+    }
+    // reset chart area
+    data = values.filter(function (d) { return d.GDP > minGDP; });
+
+    document.getElementById('chart').innerHTML = '';
+
+    var margin = { top: 60, right: 20, bottom: 30, left: 100 },
+        width = window.innerWidth - 100 - margin.left - margin.right,
+        height = 520 - margin.top - margin.bottom;
+
+    var x = d3.scaleLinear()
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var xAxis = d3.axisBottom(x);
+
+    var yAxis = d3.axisLeft(y);
+
+    var svg = d3.select("#chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    data.forEach(function (d) {
+        d['Life expectancy'] = +d['Life expectancy'];
+        d[property] = +d[property];
+        d.GDP = +d.GDP;
+    });
+
+    x.domain(d3.extent(data, function (d) { return d[property]; })).nice();
+    y.domain(d3.extent(data, function (d) { return d['Life expectancy']; })).nice();
+    var totalGDP = d3.sum(data, function (d) { return d.GDP; });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
         .attr('x', width - margin.right)
-        .attr('y', -10)
-        .text('Co2 Emissions');
-
-    svg.append('g')
-        .attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(y))
-        // Add y-axis title 'text' element.
-        .append('text')
-        .attr('transform', `translate(20, ${margin.top}) rotate(-90)`)
+        .attr("y", -10)
         .attr('text-anchor', 'end')
         .attr('fill', 'black')
         .attr('font-size', '12px')
         .attr('font-weight', 'bold')
-        .text('Life Expectancy');
+        .text(property);
 
-    const countries = svg
-        .selectAll('circle')
-        .data(data)
-        .join('circle')
-        .sort((a, b) => b.pop - a.pop) 
-        .attr('class', 'country')
-        .attr('opacity', 0.75)
-        .attr('fill', d => color(d.Country))
-        .attr('cx', d => x(d['Co2-Emissions']))  
-        .attr('cy', d => y(d['Life expectancy']))
-        .attr('r', d => size(d.GDP));
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 10)
+        .attr("dy", ".71em")
+        .attr('text-anchor', 'end')
+        .attr('fill', 'black')
+        .attr('font-size', '12px')
+        .attr('font-weight', 'bold')
+        .text("Life Expectancy")
 
-    // add a tooltip
-    countries
-        .append('title')
-        .text(d => d.Country);
+    svg.append('text')
+        .attr('x', width / 2)
+        .attr('y', -margin.top + 30)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '16px')
+        .attr('font-weight', 'bold')
+        .text(`Graph of ${property} verses Life expectancy by Country`);
 
-    // Add mouse hover interactions, using D3 to update attributes directly.
-    // In a stand-alone context, we could also use stylesheets with 'circle:hover'.
-    countries
-        // The 'on()' method registers an event listener function
-        .on('mouseover', function() {
-        // The 'this' variable refers to the underlying SVG element.
-        // We can select it directly, then use D3 attribute setters.
-        // (Note that 'this' is set when using "function() {}" definitions,
-        //  but *not* when using arrow function "() => {}" definitions.)
-        d3.select(this).attr('stroke', '#333').attr('stroke-width', 2);
-        })
-        .on('mouseout', function() {
-        // Setting the stroke color to null removes it entirely.
-        d3.select(this).attr('stroke', null);
+    var zoom = d3.zoom()
+        .scaleExtent([1, 10])
+        .translateExtent([[0, 0], [width, height],])
+        .on("zoom", function () {
+            svg.attr("transform", d3.event.transform);
+            x.call(xAxis.scale(d3.event.transform.rescaleX(x)));
+            y.call(yAxis.scale(d3.event.transform.rescaleY(y)));
         });
 
-        // Add a legend.
+    if (!document.getElementById("forth").classList.contains("hidden")) {
+        svg.call(zoom);
+    }
+
+    var legend = svg.append("g")
+        .attr("class", "legend")
+        .selectAll("g")
+        .data(color.domain())
+        .enter()
+        .append("g")
+        .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+    // Draw rects, and color them by original_index
+    legend.append("rect")
+        .attr("width", 100)
+        .attr("height", 50)
+        .style("fill", "red");
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("dy", "0.50em")
+        .text(function (d) { return d.Country; });
+
+    var filteredData = data.filter(function (d) { return (d.GDP / totalGDP) < 0.035; });
+
+    var meanX = d3.deviation(filteredData, function (d) { return x(d[property]); });
+    var meanY = d3.deviation(filteredData, function (d) { return y(d['Life expectancy']); });
+
+    var annotations = [{
+        note: {
+            label: "Annotation",
+            title: "Cluster"
+        },
+        x: meanX,
+        y: meanY,
+        dy: -50,
+        dx: 50,
+        subject: { radius: 50, radiusPadding: 10 },
+    }];
 
 
-        // Add a title.
-        svg.append('text')
-            .attr('x', width / 2)
-            .attr('y', margin.top)
-            .attr('text-anchor', 'middle')
-            .attr('font-size', '16px')
-            .attr('font-weight', 'bold')
-            .text('Top 20 Countries by GDP');
+    if (document.getElementById("forth").classList.contains("hidden")) {
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", function (d) { return Math.max(3.5, (d.GDP / (totalGDP)) * 100); })
+            .attr("cx", function (d) { return x(d[property]); })
+            .attr("cy", function (d) { return y(d['Life expectancy']); })
+            .style("fill", function (d) { return color(d.Country); })
+            var makeAnnotations = d3.annotation()
+            .annotations(annotations);
+
+        // Add the annotation to the chart
+        svg.append("g")
+            .attr("class", "annotation-group")
+            .call(makeAnnotations);
+
+        // Draw a circle around each cluster
+        svg.append("circle")
+            .attr("class", "cluster-circle")
+            .attr("cx", meanX)
+            .attr("cy", meanY)
+            .attr("r", 70)
+            .style("fill", "none")
+            .style("stroke", "black");
+    } else {
+
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", function (d) { return Math.max(3.5, (d.GDP / (totalGDP)) * 100); })
+            .attr("cx", function (d) { return x(d[property]); })
+            .attr("cy", function (d) { return y(d['Life expectancy']); })
+            .style("fill", function (d) { return color(d.Country); })
+            .on('mouseover', function () {
+                d3.select(this).attr('stroke', '#000').attr('stroke-width', 1);
+            })
+            .on('mouseout', function () {
+                d3.select(this).attr('stroke', null);
+            })
+            .append('title')
+            .text(function (d) { return `Country: ${d.Country} \n${property}: ${d[property]}\nLife Expectancy: ${d['Life expectancy']}` });
+        
+    }
 
 
-    });
+
+    if (property == 'Co2-Emissions') {
+        document.getElementById("description").innerHTML = "Description for Co2 Emissions"
+
+    } else if (property == 'Out of pocket health expenditure') {
+        document.getElementById("description").innerHTML = "Description forOut of pocket health expenditure"
+
+    } else if (property == 'Urban_population') {
+        document.getElementById("description").innerHTML = "Description for Urban_population"
+
+    } else {
+        document.getElementById("description").innerHTML = ""
+    }
+
 }
-init();
+
+
